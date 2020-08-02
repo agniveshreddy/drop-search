32a9fc8112b5aecc97e281a1e5ae5751ac13e209
Agnivesh Reddy <m.agnivesh.reddy@gmail.com>
2020-08-02 14:38:12 +0530

    commit4: adding country to the list after backend call

--- src/reducers/countriesReducer.js
+++ src/reducers/countriesReducer.js
@@ -5,6 +5,9 @@ export default function(state = [], action) {
   switch(action.type) {
     case types.GET_COUNTRIES_SUCCESS:
       return [...state, ...countries];
+    case types.ADD_COUNTRY_SUCCESS:
+      const {response: {value}}= action;
+      return [...state, value]; 
     case types.GET_COUNTRIES_ERROR:
       return { error };
     default:
